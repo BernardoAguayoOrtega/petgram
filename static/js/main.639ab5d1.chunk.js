791014e3ec4548d1301515a112365d88b07555ca
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-27 13:26:57 -0500

    Updates

---
+++ static/js/main.639ab5d1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppetgram=this.webpackJsonppetgram||[]).push([[0],{11:function(e,t,n){e.exports=n(21)},21:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),p=n(8),u=n.n(p),c=n(10),l=function(){return r.a.createElement("h1",null,"Set up")},s=document.getElementById("app");u.a.render(r.a.createElement(c.a,null,r.a.createElement(l,null)),s)}},[[11,1,2]]]);
+//# sourceMappingURL=main.639ab5d1.chunk.js.map
